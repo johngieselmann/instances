768eda967e22d2d2a51501f6e284cd71c5eb2e53
John Gieselmann <john.gieselmann@gmail.com>
2015-05-06 09:21:40 -0500

    Remove test test (whoa).

--- tests/InstancesTest.php
+++ tests/InstancesTest.php
@@ -4,10 +4,4 @@ use Codebac\Instances\Instances;
 
 class InstancesTest extends PHPUnit_Framework_TestCase {
 
-  public function testNachHasCheese()
-  {
-    $instance = new Instances;
-    $this->assertTrue($instance->doWork());
-  }
-
 }
